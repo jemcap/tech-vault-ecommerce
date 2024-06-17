f4698819df308d30be01219807a83476defcc090
JCapito <joshuaecapito22@gmail.com>
2024-06-17 15:17:19 +0100

    Added _redirects file for deployment and routing

--- src/App.js
+++ src/App.js
@@ -3,8 +3,7 @@ import { Container } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NavbarComponent from "./components/NavbarComponent";
-import Cancel from "./pages/Cancel";
-import Success from "./pages/Success";
+
 import Store from "./pages/Store";
 import CartProvider from "./context/CartContext";
 import Home from "./pages/Home";
@@ -19,10 +18,8 @@ function App() {
         <Container fluid>
           <BrowserRouter>
             <Routes>
-              <Route index element={<Home />} />
+              <Route path="/" element={<Home />} />
               <Route path="store" element={<Store />} />
-              <Route path="success" element={<Success />} />
-              <Route path="cancel" element={<Cancel />} />
             </Routes>
           </BrowserRouter>
         </Container>
